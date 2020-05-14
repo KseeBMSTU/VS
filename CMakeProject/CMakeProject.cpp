1dfa505ba7e7299acffc0b5681df9c3f951a7d0e
Ksee <k.varaksina@bmstu.ru>
2020-05-14 14:59:11 +0300

    VisualStudio

---
+++ CMakeProject/CMakeProject.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello from CMake in VS" << endl;
+	return 0;
+}
